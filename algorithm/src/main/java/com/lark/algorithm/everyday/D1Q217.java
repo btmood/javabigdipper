1262f906f07fc084ff6a4b3b2d07c441d9c5c615
btmood <1666512670@qq.com>
2021-08-20 17:03:22 +0800

    add everyday algorithm

---
+++ algorithm/src/main/java/com/lark/algorithm/everyday/D1Q217.java
@@ -0,0 +1,70 @@
+package com.lark.algorithm.everyday;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.IntStream;
+
+/**
+ * @author btmood
+ * @date 2021-08-17 17:44
+ * @Description TODO
+ * 给定一个整数数组，判断是否存在重复元素。
+ *
+ * 如果存在一值在数组中出现至少两次，函数返回 true 。如果数组中每个元素都不相同，则返回 false 。
+ *
+ *  
+ *
+ * 示例 1:
+ *
+ * 输入: [1,2,3,1]
+ * 输出: true
+ * 示例 2:
+ *
+ * 输入: [1,2,3,4]
+ * 输出: false
+ * 示例 3:
+ *
+ * 输入: [1,1,1,3,3,4,3,2,4,2]
+ * 输出: true
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/contains-duplicate
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class D1Q217 {
+    public boolean containsDuplicate1(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i +1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean containsDuplicate2(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean containsDuplicate3(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int i : nums) {
+            if (!set.add(i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean containsDuplicate4(int[] nums) {
+        return IntStream.of(nums).distinct().count() != nums.length;
+    }
+}
---
+++ algorithm/src/main/java/com/lark/algorithm/everyday/D3Q350.java
@@ -0,0 +1,66 @@
+package com.lark.algorithm.everyday;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author btmood
+ * @date 2021-08-20 17:01
+ * @Description TODO
+ * 给定两个数组，编写一个函数来计算它们的交集。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：nums1 = [1,2,2,1], nums2 = [2,2]
+ * 输出：[2,2]
+ * 示例 2:
+ *
+ * 输入：nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * 输出：[4,9]
+ *  
+ *
+ * 说明：
+ *
+ * 输出结果中每个元素出现的次数，应与元素在两个数组中出现次数的最小值一致。
+ * 我们可以不考虑输出结果的顺序。
+ * 进阶：
+ *
+ * 如果给定的数组已经排好序呢？你将如何优化你的算法？
+ * 如果 nums1 的大小比 nums2 小很多，哪种方法更优？
+ * 如果 nums2 的元素存储在磁盘上，内存是有限的，并且你不能一次加载所有的元素到内存中，你该怎么办？
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/intersection-of-two-arrays-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class D3Q350 {
+
+    public int[] intersect1(int[] nums1, int[] nums2) {
+        int[] longArr = null;
+        int[] shortArr = null;
+        if (nums1.length > nums2.length) {
+            longArr = nums1;
+            shortArr = nums2;
+        } else {
+            shortArr = nums1;
+            longArr = nums2;
+        }
+        List<Integer> indexList = new ArrayList<>();
+        for (int i = 0; i < longArr.length; i++) {
+            for (int j = 0; j < shortArr.length; j++) {
+                if (longArr[i] == shortArr[j] && !indexList.contains(j)) {
+                    indexList.add(j);
+                    break;
+                }
+            }
+        }
+        int[] res = new int[indexList.size()];
+        for (int i = 0; i < indexList.size(); i++) {
+            res[i] = shortArr[indexList.get(i)];
+        }
+        return res;
+    }
+
+}
